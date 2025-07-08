bebadd687e038b4225d6ee37cb4e7879b5137514
neeraj gupta <neerajgutagupta@gmail.com>
2025-07-08 23:44:54 +0530

    Fixed setup_view2 import for deployment

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
-from  . import views
 from django.views.generic import TemplateView
-from .views import setup_view
+from . import views
 
-urlpatterns=[
-    path('',views.home_view,name='home'),
-   
-     path('custom-text/', views.custom_text_view, name='custom_text'),
-     path("robots.txt", TemplateView.as_view(template_name="testapp/robots.txt", content_type="text/plain")),
-     path('default-text/', views.default_text_view, name='default_text'), 
-     path('googlec47f5d51e638c428.html', TemplateView.as_view(template_name="googlec47f5d51e638c428.html")),
-     path('setup/', setup_view),
+urlpatterns = [
+    path('', views.home_view, name='home'),
 
+    path('custom-text/', views.custom_text_view, name='custom_text'),
+    path('default-text/', views.default_text_view, name='default_text'),
 
+    # SEO/verification/static plain text files
+    path("robots.txt", TemplateView.as_view(template_name="testapp/robots.txt", content_type="text/plain")),
+    path("googlec47f5d51e638c428.html", TemplateView.as_view(template_name="googlec47f5d51e638c428.html", content_type="text/html")),
 
-
-
+    # Setup route (for deployment setup view)
+    path('setup/', views.setup_view, name='setup'),
 ]
-
-
